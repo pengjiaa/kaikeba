6a13c91730e2b57ca668fc954069394a172d899f
pengjia <734289052@qq.com>
2020-04-24 10:09:48 +0800

    fix: a

--- koa/serve2.js
+++ koa/serve2.js
@@ -1,32 +1,28 @@
-const Koa = require("koa")
-const Router = require("koa-router")
+const Koa = require("koa");
+const Router = require("koa-router");
 
-let serve = new Koa()
-serve.listen(2000)
+let serve = new Koa();
+serve.listen(2000);
 
-let router = new Router()
+let router = new Router();
 
-
-let aaaRouter = new Router()
-
+let aaaRouter = new Router();
 
 aaaRouter.get("/111", async ctx => {
-    ctx.body = "aaa111"
-})
+  ctx.body = "aaa111";
+});
 aaaRouter.get("/222", async ctx => {
-    ctx.body = "aaa2222"
-})
+  ctx.body = "aaa2222";
+});
 
-
-let bbbRouter = new Router()
+let bbbRouter = new Router();
 
 bbbRouter.get("/111", async ctx => {
-    ctx.body = "bbb111"
-})
+  ctx.body = "bbb111";
+});
 bbbRouter.get("/222", async ctx => {
-    ctx.body = "bbb222"
-})
-router.use("/a", aaaRouter.routes())
-router.use("/b", bbbRouter.routes())
-serve.use(router.routes())
-
+  ctx.body = "bbb222";
+});
+router.use("/a", aaaRouter.routes());
+router.use("/b", bbbRouter.routes());
+serve.use(router.routes());
